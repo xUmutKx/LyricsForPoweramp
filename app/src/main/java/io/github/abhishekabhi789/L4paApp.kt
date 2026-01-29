a9bfbd99c3da72c01c890520ce38cf1766e46b95
Abhishek <abhishekabhi9240@gmail.com>
2026-01-29 08:42:04 +0530

    Enable StrictMode in debug builds

--- app/src/main/java/io/github/abhishekabhi789/L4paApp.kt
+++ app/src/main/java/io/github/abhishekabhi789/L4paApp.kt
@@ -1,10 +1,13 @@
 package io.github.abhishekabhi789
 
 import android.app.Application
+import android.os.StrictMode
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import io.github.abhishekabhi789.lyricsforpoweramp.BuildConfig
 import javax.inject.Inject
+
 
 @HiltAndroidApp
 class L4paApp : Application(), Configuration.Provider {
@@ -12,5 +15,26 @@ class L4paApp : Application(), Configuration.Provider {
     lateinit var workerFactory: HiltWorkerFactory
     override val workManagerConfiguration: Configuration by lazy {
         Configuration.Builder().setWorkerFactory(workerFactory).build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll() // Detects all potential thread violations
+                    .penaltyLog() // Logs violations to logcat
+                    // .penaltyDialog() // Shows an annoying dialog
+                    // .penaltyDeath() // Crashes the app on violation (useful for finding hard errors)
+                    .build()
+            )
+
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+        }
     }
 }
